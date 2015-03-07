d422867d7f3c92a4bc1cd616d6aae6e44c63f81c
absimas <ab.simas@gmail.com>
2015-03-07 11:46:02 +0200

    Initial Commit

---
+++ HoverBall2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HoverBall2
+//
+//  Created by Simas Abramovas on 3/7/15.
+//  Copyright (c) 2015 Simas Abramovas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
